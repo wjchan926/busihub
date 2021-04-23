bebe3bf521efa3532a50c096e97fde034916c8d7
Wesley Chan <wesley.j.chan@gmail.com>
2021-04-23 17:28:15 -0400

    Update Website section

--- src/WebsiteContent/WebsiteContent.jsx
+++ src/WebsiteContent/WebsiteContent.jsx
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 import { Form } from "semantic-ui-react";
 
+import config from "../config/config.json";
+
 const WebsiteContent = () => {
+  const [message, setMessage] = useState("");
+  const [title, setTitle] = useState("");
+
+  const handleTitleChange = useCallback((e) => {
+    setTitle(e.target.value);
+  }, []);
+
+  const handleTextAreaChange = useCallback((e) => {
+    setMessage(e.target.value);
+  }, []);
+
+  const handlePost = async () => {
+    await fetch(config.WEBSITE.WEBSITE_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "no-cors",
+      body: JSON.stringify({
+        title,
+        content: message,
+      }),
+    });
+  };
+
   return (
     <Form>
       <Form.Field>
         <span>
-          <label>Website Url:</label> <a href="_blank">Input URL here</a>
+          <label>Website Url: </label>
+          <a href="http://www.wchan-labs.com" target="_blank" rel="noreferrer">www.wchan-labs.com</a>
         </span>
       </Form.Field>
       <Form.TextArea
+        label="Post Title:"
+        placeholder="Title of Post"
+        onChange={handleTitleChange}
+        value={title}
+      />
+      <Form.TextArea
         label="Add Website Content"
         placeholder="Content to add to Business Website"
+        onChange={handleTextAreaChange}
+        value={message}
       />
-      <Form.Button primary>
+      <Form.Button primary onClick={handlePost}>
         Post to Website
       </Form.Button>
     </Form>
